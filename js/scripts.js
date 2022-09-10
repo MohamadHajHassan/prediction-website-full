6bcba5dac09a31558f601fc5887b74dfa4b29897
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2022-09-10 09:02:22 +0300

    fetch the nationality api

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@ window.onload = () => {
   const dog = document.getElementById("dog");
   const gender = document.getElementById("gender");
   const age = document.getElementById("age");
+  const nationality = document.getElementById("nationality");
 
   // fetch the api to display a random dog picture
   fetch("https://dog.ceo/api/breeds/image/random")
@@ -20,5 +21,17 @@ window.onload = () => {
   fetch("https://api.agify.io/?name=nour")
     .then(res => res.json())
     .then(data => (age.innerHTML = `${data.age}`))
+    .catch(error => console.log(error));
+
+  // fetch the nationality api
+  fetch("https://api.nationalize.io/?name=mohamad")
+    .then(res => res.json())
+    .then(
+      data =>
+        (nationality.innerHTML =
+          data.country.length == 1
+            ? `Your predicted nationality is ${data.country[0].country_id}`
+            : `Your predicted nationalities are ${data.country[0].country_id} and ${data.country[1].country_id}`)
+    )
     .catch(error => console.log(error));
 };
