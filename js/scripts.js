ba448f4f6052b5f6d4ac25740c405f4555353e07
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2022-09-10 07:50:41 +0300

    fectch the api for dogs images

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 window.onload = () => {
-    console.log("Hello World")
-    test = document.getElementById("test")
-    test.innerText = "Hello World"
-}
+  
+  // variables
+  const dog = document.getElementById("dog");
+
+  // fetch the api to display a radom dog picture
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((res) => {return res.json();})
+    .then((data) =>(dog.innerHTML = `<img src=${data.message} alt="image of a cute dog">`))
+    .catch((error) => console.log(error));
+};
