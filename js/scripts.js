ea22528dc36acb7c354a112cc748119d7ed8c650
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2022-09-10 08:38:10 +0300

    fetch the gender api

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 window.onload = () => {
-  
   // variables
   const dog = document.getElementById("dog");
+  const gender = document.getElementById("gender");
 
-  // fetch the api to display a radom dog picture
+  // fetch the api to display a random dog picture
   fetch("https://dog.ceo/api/breeds/image/random")
-    .then((res) => {return res.json();})
-    .then((data) =>(dog.innerHTML = `<img src=${data.message} alt="image of a cute dog">`))
-    .catch((error) => console.log(error));
+    .then(res => res.json())
+    .then(data => (dog.innerHTML = `<img src=${data.message} alt="image of a cute dog">`))
+    .catch(error => console.log(error));
+
+  // fetch the gender api
+  fetch("https://api.genderize.io?name=rio")
+    .then(res => res.json())
+    .then(data => (gender.innerHTML = `${data.gender}`))
+    .catch(error => console.log(error));
 };
