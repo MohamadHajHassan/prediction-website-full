0b12730b47fa6567c54f5b8147de5f07a7cff3e4
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2022-09-10 11:04:55 +0300

    add event listener for input and submit button

--- js/scripts.js
+++ js/scripts.js
@@ -1,37 +1,61 @@
 window.onload = () => {
-  // variables
+  // Declare Variables
   const dog = document.getElementById("dog");
   const gender = document.getElementById("gender");
   const age = document.getElementById("age");
   const nationality = document.getElementById("nationality");
+  const button = document.getElementById("submit");
+  const nameInput = document.getElementById("name");
+  let name;
 
-  // fetch the api to display a random dog picture
-  fetch("https://dog.ceo/api/breeds/image/random")
-    .then(res => res.json())
-    .then(data => (dog.innerHTML = `<img src=${data.message} alt="image of a cute dog">`))
-    .catch(error => console.log(error));
+  // Functions
+  // fetch the api to display a random dog picture function
+  let fetchDog = () => {
+    return fetch("https://dog.ceo/api/breeds/image/random")
+      .then(res => res.json())
+      .then(data => (dog.innerHTML = `<img src=${data.message} alt="image of a cute dog">`))
+      .catch(error => console.log(error));
+  };
 
-  // fetch the gender api
-  fetch("https://api.genderize.io?name=rio")
-    .then(res => res.json())
-    .then(data => (gender.innerHTML = `${data.gender}`))
-    .catch(error => console.log(error));
+  // fetch the gender api function for input name
+  let fetchGender = name => {
+    return fetch(`https://api.genderize.io?name=${name}`)
+      .then(res => res.json())
+      .then(data => (gender.innerHTML = `Your predicted gender is: ${data.gender}`))
+      .catch(error => console.log(error));
+  };
 
-  // fetch the age api
-  fetch("https://api.agify.io/?name=nour")
-    .then(res => res.json())
-    .then(data => (age.innerHTML = `${data.age}`))
-    .catch(error => console.log(error));
+  // fetch the age api function for input name
+  let fetchAge = name => {
+    return fetch(`https://api.agify.io/?name=${name}`)
+      .then(res => res.json())
+      .then(data => (age.innerHTML = `Your predicted age is: ${data.age}`))
+      .catch(error => console.log(error));
+  };
 
-  // fetch the nationality api
-  fetch("https://api.nationalize.io/?name=mohamad")
-    .then(res => res.json())
-    .then(
-      data =>
-        (nationality.innerHTML =
-          data.country.length == 1
-            ? `Your predicted nationality is ${data.country[0].country_id}`
-            : `Your predicted nationalities are ${data.country[0].country_id} and ${data.country[1].country_id}`)
-    )
-    .catch(error => console.log(error));
+  // fetch the nationality api function for input name
+  let fetchNationality = name => {
+    return fetch(`https://api.nationalize.io/?name=${name}`)
+      .then(res => res.json())
+      .then(
+        data =>
+          (nationality.innerHTML =
+            data.country.length == 1
+              ? `Your predicted nationality is ${data.country[0].country_id}`
+              : `Your predicted nationalities are ${data.country[0].country_id} and ${data.country[1].country_id}`)
+      )
+      .catch(error => console.log(error));
+  };
+
+  //
+  //
+  //
+  fetchDog();
+  nameInput.addEventListener("input", event => (name = event.target.value));
+  console.log(name);
+  button.addEventListener("click", () => {
+    fetchAge(name);
+    fetchGender(name);
+    fetchNationality(name);
+  });
 };
