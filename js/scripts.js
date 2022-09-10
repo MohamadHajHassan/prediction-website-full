b75fb54c1a7fa624189f1b051e8cd140aba5029d
Mohamad Haj Hassan <mohamadhajhassan33@gmail.com>
2022-09-10 08:40:35 +0300

    fetch the age api

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,7 @@ window.onload = () => {
   // variables
   const dog = document.getElementById("dog");
   const gender = document.getElementById("gender");
+  const age = document.getElementById("age");
 
   // fetch the api to display a random dog picture
   fetch("https://dog.ceo/api/breeds/image/random")
@@ -13,5 +14,11 @@ window.onload = () => {
   fetch("https://api.genderize.io?name=rio")
     .then(res => res.json())
     .then(data => (gender.innerHTML = `${data.gender}`))
+    .catch(error => console.log(error));
+
+  // fetch the age api
+  fetch("https://api.agify.io/?name=nour")
+    .then(res => res.json())
+    .then(data => (age.innerHTML = `${data.age}`))
     .catch(error => console.log(error));
 };
